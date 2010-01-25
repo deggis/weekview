e74d755c9d100bb657c585af6687db5b1941ec98
Heikki Salo <heikki.ao.salo@iki.fi>
2010-01-25 22:35:06 +0200

    Improved data_importer script a little.

--- data_importer.py
+++ data_importer.py
@@ -11,18 +11,22 @@ def get_categories(filename, username):
 	for line in file.readlines():
 		name = line.split('|')[0]
 		if not name in catdict:
-			category = EventCategory.objects.get(name__iexact=name) # FIXME: add user check
-			if category is not None:
+			try:
+				category = EventCategory.objects.get(name__iexact=name) # FIXME: add user check
 				catdict[name.lower()] = category
-			else:
+			except Exception as inst:
+				print "Category '"+name+"' was not found!"
 				raise Exception('Error in data', 'DB does not contain category: '+name)
 	return catdict
 
 def parse_date(str):
 	datestr, timestr = str.split(' ')
-	day,month,year = datestr.split('.')
+	year,month,day = datestr.split('.')
 	hour,minute = timestr.split(':')
-	return datetime.datetime(int(year),int(month),int(day),int(hour),int(minute),0)
+	try:
+		return datetime.datetime(int(year),int(month),int(day),int(hour),int(minute),0)
+	except Exception as inst:
+		print "Invalid datestring: '"+str+"', got: "+inst.__str__()
 
 def get_user(username):
 	return User.objects.get(username__iexact=username)
@@ -31,6 +35,9 @@ def import_file(filename, username):
 	catdict = get_categories(filename, username)
 	user = get_user(username)
 	file = open(filename)
+	print """Beginning to import data. All needed categories were found!
+
+"""
 	for line in file.readlines():
 		line = line.replace("\n", '')
 		catstr, beginstr, endstr = line.split('|')
@@ -40,7 +47,12 @@ def import_file(filename, username):
 		event.category = catdict[catstr.lower()]
 		event.begin = parse_date(beginstr)
 		event.end = parse_date(endstr)
-		print event.__str__() + "\n"
+		print event.__str__()
 		event.save()
+	print """
+
+There was many datas and the datas were good!
+
+Byebye."""
 
 import_file('data.txt', 'deggis')
